20892ab78d7d3091690bb31a71aca04a7c7ccb37
xinxin8816 <xinxin8816@gmail.com>
2019-04-13 16:52:57 +0800

    fix bug

--- nfc.py
+++ nfc.py
@@ -4,10 +4,16 @@ Created on 2019-4-13 08:36:37
 @author: xinxin8816
 """
 import os
+import numpy as np
 import re
 
 nfcdata = os.popen('nfc-list')
 type(nfcdata)
-print(nfcdata.readlines())
-arr = nfcdata.readlines().split(',')
-print(arr[3])
+line = nfcdata.readlines()
+data_list = []
+while line:
+    num = list(map(float,line.split()))
+    data_list.append(num)
+    line = nfcdata.readline()
+data_array = np.array(data_list)
+print(data_array[3])
