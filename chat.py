16e647f2bcca6f12212bdd7a10e134c2d7085abb
xinxin8816 <xinxin8816@gmail.com>
2019-04-14 10:16:10 +0800

    fix database format code

--- chat.py
+++ chat.py
@@ -1,27 +1,53 @@
-#coding:utf-8
+# coding:utf-8
 """
 Created on 2018-4-10 9:58:40
 @author: xinxin8816
 """
 import network
 import sys
+import csv
+import pandas as pd
+
+database_PATH = 'database.csv'
+
+
+def adddata(outer_id, displayname, lockright):#数据库增加新行
+    row = [outer_id, displayname, lockright]
+    out = open(database_PATH, "a", newline="")
+    csv_writer = csv.writer(out, dialect="excel")
+    csv_writer.writerow(row)
+    return csv_writer
+
+def deldata(outer_id):#数据库删除行
+    odata = pd.read_csv(database_PATH)
+    with open(database_PATH, "r", encoding="ANSI") as database:
+        reader = csv.DictReader(database)
+        name = [row['outer_id'] for row in reader]
+    print(name)
+    site = name.index(outer_id)
+    odata.drop(odata.index[site], inplace=True)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("主机模式")
+        print("Server Mode")
     else:
-        print("客户端模式")
+        print("Client Mode")
+
 
 def heard(phrase):
-	print("them:" + phrase)
+    print("Receive:" + phrase)
+    print(phrase.split())
+    if phrase.split()[0] == 'doing':
+        sys.exit(0)
+
 
 if (len(sys.argv) >= 2):
-	network.call(sys.argv[1], whenHearCall=heard)
+    network.call(sys.argv[1], whenHearCall=heard)
 else:
-	network.wait(whenHearCall=heard)
+    network.wait(whenHearCall=heard)
 
 while network.isConnected():
-	phrase = raw_input() #python2
-	#phrase = input()
-	print("me:" + phrase)
-	network.say(phrase)
+    phrase = raw_input()  # python2
+    # phrase = input()
+    print("Send:" + phrase)
+    network.say(phrase)
--- nfc.py
+++ nfc.py
@@ -4,16 +4,9 @@ Created on 2019-4-13 08:36:37
 @author: xinxin8816
 """
 import os
-import numpy as np
 import re
 
 nfcdata = os.popen('nfc-list')
 type(nfcdata)
-line = nfcdata.readlines()
-data_list = []
-while line:
-    num = list(map(float,line.split()))
-    data_list.append(num)
-    line = nfcdata.readline()
-data_array = np.array(data_list)
-print(data_array[3])
+data = nfcdata.readlines()
+print(nfcdata.readlines()[3])
